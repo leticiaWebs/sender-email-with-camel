2799c33bd1de32d3fccbb13b06a2559e4bf7943c
leticia_abraao <leticiaaod@gmail.com>
2026-01-22 12:23:49 -0300

    ajuste rota

--- email-service/src/main/java/com/enviodeemail/email_service/camel/camelProcessor/SenderEmailProcessor.java
+++ email-service/src/main/java/com/enviodeemail/email_service/camel/camelProcessor/SenderEmailProcessor.java
@@ -1,35 +1,18 @@
 package com.enviodeemail.email_service.camel.camelProcessor;
 
 import com.enviodeemail.email_service.domain.Email;
-import com.enviodeemail.email_service.service.EmailService;
-import lombok.AllArgsConstructor;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class SenderEmailProcessor implements Processor {
-
-    private final JavaMailSender mailSender;
-
-    public SenderEmailProcessor(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
 
     @Override
     public void process(Exchange exchange) throws Exception {
-
         Email email = exchange.getIn().getBody(Email.class);
-
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email.getTo());
-        message.setSubject(email.getSubject());
-        message.setText(email.getBody());
-
-        mailSender.send(message);
+        System.out.println("Mock: Email enviado para " + email.getTo()
+                + " | Assunto: " + email.getSubject() + " | Corpo: " + email.getBody());
     }
 }
 
